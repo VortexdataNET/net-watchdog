77a4f3ba8ab342090668b0ae0600cabe6f0d4709
TAXSET <sandro.kierner@gmail.com>
2020-07-30 12:15:09 +0200

    Adding unit test directory and base class

---
+++ src/test/java/net/vortexdata/netwatchdog/RestComponentTest.java
@@ -0,0 +1,15 @@
+package net.vortexdata.netwatchdog;
+
+import net.vortexdata.netwatchdog.modules.component.ComponentManager;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import sun.nio.ch.Net;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class RestComponentTest {
+
+
+}
