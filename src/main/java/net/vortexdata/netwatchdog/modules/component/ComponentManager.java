942439acb1beb091894953110251bf8c06ecff3b
Sandro Kierner <sandro.kierner@gmail.com>
2020-08-22 12:22:40 +0200

    Adding JavaDoc to all classes

--- src/main/java/net/vortexdata/netwatchdog/modules/component/ComponentManager.java
+++ src/main/java/net/vortexdata/netwatchdog/modules/component/ComponentManager.java
@@ -1,3 +1,27 @@
+/*
+ * NET Watchdog
+ *
+ * Copyright (c) 2020 VortexdataNET
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package net.vortexdata.netwatchdog.modules.component;
 
 import net.vortexdata.netwatchdog.NetWatchdog;
@@ -10,6 +34,13 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Owner class of all loaded components. Manages and loads components.
+ *
+ * @author  Sandro Kierner
+ * @since   0.0.0
+ * @version 0.0.0
+ */
 public class ComponentManager {
 
     public static final String COMPONENTS_DIR = "components//";
--- src/main/java/net/vortexdata/netwatchdog/modules/component/PerformanceClass.java
+++ src/main/java/net/vortexdata/netwatchdog/modules/component/PerformanceClass.java
@@ -1,3 +1,27 @@
+/*
+ * NET Watchdog
+ *
+ * Copyright (c) 2020 VortexdataNET
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package net.vortexdata.netwatchdog.modules.component;
 
 import net.vortexdata.netwatchdog.NetWatchdog;
@@ -14,6 +38,13 @@ import java.net.SocketTimeoutException;
 import java.nio.Buffer;
 import java.util.ArrayList;
 
+/**
+ * Base class for performance classes.
+ *
+ * @author  Sandro Kierner
+ * @since   0.0.0
+ * @version 0.0.0
+ */
 public class PerformanceClass {
 
     private final String name;
