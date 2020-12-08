feaf839ea9d7b68e1aa76118382b0320556cf605
Release CI/CD <runner@fv-az58-353.f2j1iw4xqcpuxdcjd5axlwfa3h.cx.internal.cloudapp.net>
2020-12-08 15:00:02 +0000

    Preparing branch for release 0.2.0

--- src/main/java/net/vortexdata/netwatchdog/modules/northstar/ICMPNorthstar.java
+++ src/main/java/net/vortexdata/netwatchdog/modules/northstar/ICMPNorthstar.java
@@ -13,7 +13,7 @@ import java.util.regex.Pattern;
  * ICMP Northstar class.
  *
  * @author Sandro Kierner
- * @version 0.1.1
+ * @version 0.2.0
  * @since 0.1.0
  */
 public class ICMPNorthstar extends NorthstarBase {
